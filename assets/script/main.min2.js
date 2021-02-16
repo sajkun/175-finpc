258f38cbf952f5ca7604457b3225e7335bede027
sajkun <sajkunrnd@gmail.com>
2021-02-16 15:34:02 +0300

    v 1.0.2 js fix

--- assets/script/main.min2.js
+++ assets/script/main.min2.js
@@ -1056,6 +1056,10 @@ if(document.getElementById('video-items')){
 
     computed: {
       by_category: function(){
+
+        if(!video_categories || video_categories.length ==0){
+          return [];
+        }
         var vm = this;
         var items = video_items.filter(e=>{
           return e.category.indexOf(vm.current_category) >=0;
